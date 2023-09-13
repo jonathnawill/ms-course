6a80ea75bd3c4ac6a2f5e2da569fcd90f1c6d94c
Jonathan William <Jjoninha11@hotmail.com>
2023-09-13 15:55:46 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -25,12 +26,13 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-
 	private Set<Role> roles = new HashSet<>();
 
 	public User() {
